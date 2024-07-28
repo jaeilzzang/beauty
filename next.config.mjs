a2260f1bea004296efc8ebc15084981c45d05f94
jaeilzzang <jaeilju5@gmail.com>
2024-07-29 01:42:10 +0900

    빌드에러

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/app/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
